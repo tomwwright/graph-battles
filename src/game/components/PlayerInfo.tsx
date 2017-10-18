e36173ac848fc062ea3dd81fb3f4fe22f4d2753f
Tom Wright <tom.w.wright@live.com>
2017-10-18 22:27:00 +1100

    Add victory points to player info

--- src/game/components/PlayerInfo.tsx
+++ src/game/components/PlayerInfo.tsx
@@ -1,8 +1,8 @@
-import * as React from "react";
-import { Small, Text } from "rebass";
-import InfoPane from "game/components/InfoPane";
-import Player from "models/player";
-import { TerritoryAction, TerritoryActionDefinitions, ColourStrings } from "models/values";
+import * as React from 'react';
+import { Small, Text } from 'rebass';
+import InfoPane from 'game/components/InfoPane';
+import Player from 'models/player';
+import { TerritoryAction, TerritoryActionDefinitions, ColourStrings } from 'models/values';
 
 type PlayerInfoProps = {
   player: Player;
@@ -13,14 +13,15 @@ const PlayerInfo: React.StatelessComponent<PlayerInfoProps> = ({ player, isActiv
   return (
     <InfoPane>
       <Text color={ColourStrings[player.data.colour]}>
-        Player {player.data.id} <Small color="black">{isActive ? "(Active)" : ""}</Small>
+        Player {player.data.id} <Small color="black">{isActive ? '(Active)' : ''}</Small>
       </Text>
       <Small>
         <Text>
           Gold {player.data.gold} (+{player.data.goldProduction +
             player.territories.map(territory => territory.data.goldProduction).reduce((a, b) => a + b, 0)})
         </Text>
-        <Text>{player.data.ready ? "Ready" : "Not Ready"}</Text>
+        <Text>Victory Points {player.victoryPoints}</Text>
+        <Text>{player.data.ready ? 'Ready' : 'Not Ready'}</Text>
       </Small>
     </InfoPane>
   );
