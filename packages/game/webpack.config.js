68e84a8be8d3714893bf3c3ec705673c23189111
Tom Wright <tom.suffern.wright@gmail.com>
2021-10-11 15:31:47 +1100

    Module resolution

--- packages/game/webpack.config.js
+++ packages/game/webpack.config.js
@@ -1,48 +1,48 @@
-var path = require("path");
-var webpack = require("webpack");
-var glob = require("glob");
+var path = require('path');
+var webpack = require('webpack');
+var glob = require('glob');
 
 module.exports = {
   entry: {
-    lobby: ["babel-polyfill", "./build/es5/lobby/lobby.js"],
-    game: ["babel-polyfill", "./build/es5/game/game.js"],
-    vendor: ["react", "react-dom", "mobx", "mobx-react", "phaser-ce", "rebass"]
+    lobby: ['babel-polyfill', './build/es5/lobby/lobby.js'],
+    game: ['babel-polyfill', './build/es5/game/game.js'],
+    vendor: ['react', 'react-dom', 'mobx', 'mobx-react', 'phaser-ce', 'rebass'],
   },
 
   // Enable sourcemaps for debugging webpack's output.
-  devtool: "source-map",
+  devtool: 'source-map',
 
   resolve: {
-    extensions: [".webpack.js", ".web.js", ".js"],
+    extensions: ['.webpack.js', '.web.js', '.js'],
 
-    modules: ["node_modules", path.resolve(__dirname, "build/es5")],
+    modules: ['node_modules', path.resolve(__dirname, 'build/es5')],
 
     alias: {
-      pixi: path.join(__dirname, "node_modules/phaser-ce/build/custom/pixi.js"),
-      "phaser-ce": path.join(__dirname, "node_modules/phaser-ce/build/custom/phaser-split.js"),
-      p2: path.join(__dirname, "node_modules/phaser-ce/build/custom/p2.js")
-    }
+      pixi: path.join(__dirname, '../../node_modules/phaser-ce/build/custom/pixi.js'),
+      'phaser-ce': path.join(__dirname, '../../node_modules/phaser-ce/build/custom/phaser-split.js'),
+      p2: path.join(__dirname, '../../node_modules/phaser-ce/build/custom/p2.js'),
+    },
   },
 
   module: {
     rules: [
-      { test: /\.js$/, loader: "source-map-loader" }, // re-process existing source maps
-      { test: /pixi\.js$/, loader: "expose-loader?PIXI" },
-      { test: /phaser-split\.js$/, loader: "imports-loader?pixi,p2!expose-loader?Phaser" },
-      { test: /p2\.js$/, loader: "expose-loader?p2" }
-    ]
+      { test: /\.js$/, loader: 'source-map-loader' }, // re-process existing source maps
+      { test: /pixi\.js$/, loader: 'expose-loader?PIXI' },
+      { test: /phaser-split\.js$/, loader: 'imports-loader?pixi,p2!expose-loader?Phaser' },
+      { test: /p2\.js$/, loader: 'expose-loader?p2' },
+    ],
   },
 
   plugins: [
-    new webpack.ExternalsPlugin("commonjs", ["aws-sdk"]),
+    new webpack.ExternalsPlugin('commonjs', ['aws-sdk']),
     new webpack.optimize.CommonsChunkPlugin({
-      names: ["model", "vendor"],
-      filename: "[name].bundle.js"
-    })
+      names: ['model', 'vendor'],
+      filename: '[name].bundle.js',
+    }),
   ],
 
   output: {
-    path: __dirname + "/build/bundle",
-    filename: "[name].bundle.js"
-  }
+    path: __dirname + '/build/bundle',
+    filename: '[name].bundle.js',
+  },
 };
