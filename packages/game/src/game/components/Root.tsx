726cdb89a31dea0dda2437f49785fbf78b5a0e32
Tom Wright <tom.suffern.wright@gmail.com>
2021-11-09 23:47:54 +1100

    Split UI into two columns

--- packages/game/src/game/components/Root.tsx
+++ packages/game/src/game/components/Root.tsx
@@ -1,11 +1,15 @@
-import * as React from "react";
-import SelectedInfo from "game/components/SelectedInfo";
-import GameInfo from "game/components/GameInfo";
+import * as React from 'react';
+import SelectedInfo from 'game/components/SelectedInfo';
+import GameInfo from 'game/components/GameInfo';
 
 const Root: React.StatelessComponent<{}> = () => (
   <div>
-    <GameInfo />
-    <SelectedInfo />
+    <div style={{ position: 'absolute', left: 0, top: 0, margin: '8px', padding: 0 }}>
+      <GameInfo />
+    </div>
+    <div style={{ position: 'absolute', right: 0, top: 0, margin: '8px', padding: 0 }}>
+      <SelectedInfo />
+    </div>
   </div>
 );
 
