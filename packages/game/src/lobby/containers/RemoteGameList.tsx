aff2e40d800399ecc64d073e3f0f8c290a5b128a
Tom Wright <tom.suffern.wright@gmail.com>
2022-01-06 09:33:44 +1100

    Add filtering by user to the remote games list

--- packages/game/src/lobby/containers/RemoteGameList.tsx
+++ packages/game/src/lobby/containers/RemoteGameList.tsx
@@ -9,14 +9,20 @@ type RemoteGameListProps = {
   remoteGameStore?: RemoteGameStore;
   userId: string;
 };
-const RemoteGameListComponent: React.StatelessComponent<RemoteGameListProps> = ({ remoteGameStore, userId }) => (
-  <div>
-    {remoteGameStore.games.length == 0 ? (
-      <Text>No games found.</Text>
-    ) : (
-      remoteGameStore.games.map((game) => <RemoteGameComponent key={game.gameId} game={game} userId={userId} />)
-    )}
-  </div>
-);
+const RemoteGameListComponent: React.StatelessComponent<RemoteGameListProps> = ({ remoteGameStore, userId }) => {
+  const gamesWithUser = remoteGameStore.games.filter((game) =>
+    game.leaderboard.map((leader) => leader.name).includes(userId)
+  );
+
+  return (
+    <div>
+      {gamesWithUser.length == 0 ? (
+        <Text>No games found.</Text>
+      ) : (
+        gamesWithUser.map((game) => <RemoteGameComponent key={game.gameId} game={game} userId={userId} />)
+      )}
+    </div>
+  );
+};
 
 export const RemoteGameList = inject('remoteGameStore')(observer(RemoteGameListComponent));
