5a9cca4c2fdaa38edc33f06726b6e5136123c535
Tom Wright <tom.suffern.wright@gmail.com>
2026-04-12 17:37:39 +1000

    ci: deploy @battle/gamev2 at /v2/

--- packages/ops/lib/pipeline-stack.ts
+++ packages/ops/lib/pipeline-stack.ts
@@ -40,6 +40,8 @@ export class PipelineStack extends cdk.Stack {
           "yarn workspace @battles/api test",
           "yarn workspace @battles/game bundle:client",
           "yarn workspace @battles/game package",
+          "yarn workspace @battles/gamev2 build",
+          "yarn workspace @battles/gamev2 package",
           "yarn workspace @battles/ops cdk synth",
         ],
         primaryOutputDirectory: "packages/ops/cdk.out",
